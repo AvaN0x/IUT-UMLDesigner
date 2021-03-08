430443316d23b1a237b2964034859c7a295e71ca
AvaN0x <clement.ricatte57@gmail.com>
2021-03-08 15:28:58 +0100

    added a small test for Queue

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
+#include <iostream>
+
 #include "mainwindow.h"
 
 #include <QApplication>
+
+#include "Queue.hpp"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    iut_cpp::Queue<int> tqueue;
+    tqueue.push(1);
+    tqueue.push(2);
+    tqueue.push(3);
+    tqueue.push(5);
+    tqueue.push(9);
+    tqueue.pop();
+    std::cout << tqueue << std::endl;
+
     return a.exec();
 }
