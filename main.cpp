226c6e36ad258ea5b4a2895ffa8139d7ac65479c
AvaN0x <clement.ricatte57@gmail.com>
2021-03-12 13:52:06 +0100

    we can now create a file for the class

--- main.cpp
+++ main.cpp
@@ -14,29 +14,30 @@
 #include "models/Method.hpp"
 #include "models/Argument.hpp"
 
-/**
- * @brief  Write a content into a file at a path
- * @param  path: must contain extension at the end of file
- * @param  wrapper: content to write
- * @return bool, success or error
- */
-bool writeInFile(std::string const &path, iut_cpp::Wrapper const &wrapper)
+bool createAndWriteFile(std::string const &fullPath, iut_cpp::Wrapper const &wrapper)
 {
     std::ofstream file;
-
-    //TODO generate file name with extension
-    file.open(path);
+    file.open(fullPath);
     if (!file)
     {
-        std::cerr << "Error: could not open file at path : " << path << std::endl;
+        std::cerr << "Error: could not open file at path : " << fullPath << std::endl;
         return false;
     }
 
     file << wrapper;
-    std::cout << "Class created at path : " << path << std::endl;
+    std::cout << "Class created at path : " << fullPath << std::endl;
     file.close();
 
     return true;
+}
+
+bool createClass(std::string const &path, iut_cpp::Class const &c, std::string const &language)
+{
+    if (language == "java")
+        return createAndWriteFile(path + c._name + ".java", c.toJava());
+
+    std::cerr << "Error: Language not found : " << language << std::endl;
+    return false;
 }
 
 int main(int argc, char *argv[])
@@ -62,12 +63,12 @@ int main(int argc, char *argv[])
     arguments2.push_last(iut_cpp::Argument("z", "int", "0"));
 
     iut_cpp::List<iut_cpp::Method> methods;
-    methods.push_last(iut_cpp::Method("somme", "int", "public", true, arguments1));
+    methods.push_last(iut_cpp::Method("somme", "int", "public", false, arguments1));
     methods.push_last(iut_cpp::Method("hypothenuse", "int", "public", true, arguments2));
 
     iut_cpp::Class c1("TestClass", attributes, true, true, methods);
 
-    writeInFile("Test.java", c1.toJava());
+    createClass("", c1, "java");
 
     return a.exec();
 }
